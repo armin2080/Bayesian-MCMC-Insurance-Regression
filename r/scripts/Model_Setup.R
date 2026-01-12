a8f06b65c103d8fb04c1f59fc6b93307328e4898
Tara918 <rahmani.tmail@yahoo.com>
2026-01-12 22:19:45 +0100

    Update model setup to support ACF, ESS

--- r/scripts/Model_Setup.R
+++ r/scripts/Model_Setup.R
@@ -1,6 +1,4 @@
-
-
-## (2) Design matrix
+# (2.1) Design matrix
 # response:
 y <- expenses_clean$charges
 
@@ -12,22 +10,22 @@ x <- model.matrix(
 n <- nrow(x)
 p <- ncol(x)
 
-# Baseline Frequentist Fit:
+# (2.2) Baseline Frequentist Fit:
 ols_fit <- lm(
   charges ~ age + sex + bmi + children + smoker,
-  data = expenses_clean
-)
+  data = expenses_clean 
+) # the effect of "sex" is non-significant
 
 sink("ols_output.txt")
 summary(ols_fit)
 sink()
 
-# correlation matrix:
+#(2.3) correlation matrix:
 cor_matrix <- cor(num_expenses)
 sink("cor_matrix.txt")
 cor_matrix
 sink()
-
+# "sex" has a very small correlation with "charges"
 
 #------------------------------------
 # Run Gibbs Sampler & Trace Plot
@@ -54,4 +52,21 @@ sigma2_trace_plot(sigma2_list,"gibbs_result")
 
 beta_summary_stats(beta_list)
 sigma2_summary_stats(sigma2_list)
+
+#-------------------------------------------
+# Run Correlation plots
+#-------------------------------------------
+
+source("r/scripts/Convergence_Detection.R")
+
+# ACF plots
+acf_plot_beta(beta_list,"gibbs_result")
+acf_plot_sigma2(sigma2_list, "gibbs_result")
+
+# ESS
+ess_beta_table(beta_list, x, "gibbs_result")
+ess_sigma2_table(sigma2_list, "gibbs_result")
+
+
+
 
